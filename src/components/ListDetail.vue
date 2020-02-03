5ce0e3f69571cb79082e273bfc147e18fde12d27
akondo06 <akondo.ionutz@gmail.com>
2020-02-03 02:54:44 +0100

    dummy data

--- src/components/ListDetail.vue
+++ src/components/ListDetail.vue
@@ -14,6 +14,59 @@
 		</div>
 		<div class="detail">
 			<div class="inner">
+				<h1 class="title is-4">Order #2023910208</h1>
+				<h2 class="subtitle is-6">Created on 12 August 2020 16:20</h2>
+
+				<div class="columns">
+					<div class="column">
+						<div class="card">
+							<div class="card-content">
+								<p class="title is-5">
+									Sender
+								</p>
+								<b-field label="Name">
+									<b-input v-model="sender.name"></b-input>
+								</b-field>
+								<b-field label="Phone">
+									<b-input v-model="sender.phone"></b-input>
+								</b-field>
+								<b-field label="Address">
+									<b-input v-model="sender.address"></b-input>
+								</b-field>
+							</div>
+						</div>
+					</div>
+					<div class="column">
+						<div class="card">
+							<div class="card-content">
+								<p class="title is-5">
+									Receiver
+								</p>
+								<b-field label="Name">
+									<b-input v-model="receiver.name"></b-input>
+								</b-field>
+								<b-field label="Phone">
+									<b-input v-model="receiver.phone"></b-input>
+								</b-field>
+								<b-field label="Address">
+									<b-input v-model="receiver.address"></b-input>
+								</b-field>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="card-content">
+						<p class="title is-5">
+							Package
+						</p>
+						<b-field label="Weight (in kg)">
+							<b-input v-model="weight" type="number"></b-input>
+						</b-field>
+					</div>
+				</div>
+
 				PUT SOMETHING IMPORTANT HEIR!
 				<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
 				<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
@@ -28,6 +81,22 @@ import { mapState } from 'vuex';
 
 export default {
 	name: 'ListDetail',
+
+	data() {
+		return {
+			sender: {
+				name: 'Gregor Bennedict',
+				phone: '0732039208291',
+				address: 'Geneesgade 1, 8700 Horsens, Denmark'
+			},
+			receiver: {
+				name: 'Gregor Bennedict',
+				phone: '0732039208291',
+				address: 'Geneesgade 1, 8700 Horsens, Denmark'
+			},
+			weight: 30
+		};
+	},
 	computed: {
 		...mapState([
 			'orders'
